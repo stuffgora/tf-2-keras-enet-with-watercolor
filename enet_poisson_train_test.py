2dbe983686af6c969c3f05b2d1b77752a5c1c3e6
ddd <you@example.com>
2020-03-11 16:48:18 +0200

    added model preload

--- enet_poisson_train_test.py
+++ enet_poisson_train_test.py
@@ -19,74 +19,99 @@ from src.enetcfg import EnetCfg
 from PIL import Image
 from src.watercolor02_np_fft import to_water_collor, my_img_show
 
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
 flags = EnetCfg()
 flags.default_enet_cfg(flags=flags)
 cfg = flags.parse_args()
 
+
 def rand_img(m,n):
 #    img = '../stars_clean.png'    
 #    x = np.array(Image.open(img).convert("RGB"))/256.0 
-    for i in range(1000):
+    #for i in range(100000):
+    while True:
         x = np.random.rand(m,n,3)
         y = to_water_collor(x)
         yield (x,y)
-    
-    
+
+def get_rand_data(m,n):
+    return tf.data.Dataset.from_generator(
+            rand_img, 
+            output_types=(tf.float32, tf.float32),
+            output_shapes=(tf.TensorShape([m,n,3]), tf.TensorShape([m,n,3])),
+            args=(m,n)
+            )
+
+
+def get_optimizer():
+    initial_learning_rate = 5e-4 #0.1
+    #decay_steps = int(num_epochs_before_decay * num_steps_per_epoch) ~100*100
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate,
+        decay_steps=500, #10*2*(50*10),#    (steps_in_s*batch)
+        decay_rate=1e-1, #0.96,
+        staircase=True)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule, epsilon=1e-8)
+    #optimizer = tf.optimizers.Adadelta(learning_rate=5e-5, rho=1e-1, epsilon=1e-08)
+    #optimizer='adadelta'
+    return optimizer
+   
 def get_model(cfg=cfg):
     dw = cfg.image_width
     dh = cfg.image_height
     
-    optimizer = 'adam' #get_optimizer()
+    optimizer = get_optimizer() #'SGD' # 'adam' #
     loss = 'mean_squared_error' #get_loss()
     metrics=['accuracy', 'mean_squared_error'] #get_metrics()
     
-    model, model_name = autoencoder_wc(nc=3, input_shape=(dw, dh),
-                                         loss=loss,
-                                         optimizer=optimizer, 
-                                         metrics=metrics, 
-                                         #wc_in_encoder=cfg.wc_in_encoder,
-                                         #wc_in_decoder=cfg.wc_in_decoder
-                                         )
+    model, model_name = autoencoder_wc(nc=3, 
+                                       input_shape=(dw, dh),
+                                       loss=loss,
+                                       optimizer=optimizer, 
+                                       metrics=metrics )                                      
     return model
+
 
 def train( ):
     print(f'Preparing to train on {cfg.dataset_name} data...')
     
     autoenc = get_model()
-    #autoenc, ckpt = model_preload(autoenc) 
-    #train_ds, val_ds = get_train_val_data(dataset = cfg.dataset_name)
+    #model.save('saved_model/poisson_model')
+    pre_trained_model = tf.keras.models.load_model('saved_model/poisson_model')
     
-    # Class Waight balancing        
-    #cw_d = get_class_normalization_dic(dataset=cfg.dataset_name)
-    #checkpoint_dir, log_dir = get_checkpoint_log_dir()
-    # checkpoint for epoch counter
-#    if cfg.initial_epoch is None:
-#        completed_epochs = ckpt.completed_epochs.numpy()
-#    else :
-#        completed_epochs = cfg.initial_epoch  
+    autoenc.set_weights(pre_trained_model.get_weights())
+ 
     m = cfg.image_width
     n = cfg.image_height
     
-    train_ds = tf.data.Dataset.from_generator(rand_img, 
-                                              output_types=(tf.float32, tf.float32),
-                                              output_shapes=(tf.TensorShape([m,n,3]), tf.TensorShape([m,n,3])),
-                                              args=(m,n)
-                                              )
-    val_ds = tf.data.Dataset.from_generator(rand_img, 
-                                              output_types=(tf.float32, tf.float32),
-                                              output_shapes=(tf.TensorShape([m,n,3]), tf.TensorShape([m,n,3])),
-                                              args=(m,n)
-                                              )
+    train_ds = get_rand_data(m,n)
+    val_ds   = get_rand_data(m,n)
+    
     # ---------- Fit Model - Training-----------------
     history = autoenc.fit(
-        x=train_ds.batch(100),
-        epochs=cfg.epochs,
-        steps_per_epoch=cfg.steps,
+        x=train_ds.batch(5),
+        epochs=100, #200, #cfg.epochs,
+        steps_per_epoch=10, #cfg.steps,
         #class_weight=cw_d,
         verbose=1,
         #callbacks=callbacks(log_dir, checkpoint_dir, cfg.model_name ),        
-        validation_data=val_ds.batch(10),
-        validation_steps=cfg.val_steps,
+        validation_data=val_ds.batch(2),
+        validation_steps=4 #cfg.val_steps,
         #initial_epoch=completed_epochs
     )  
     print('\nhistory dict:', history.history)
@@ -94,24 +119,36 @@ def train( ):
 
 
 if __name__ == '__main__':
+    cfg.image_width  = 256
+    cfg.image_height = 256
     m = cfg.image_width
     n = cfg.image_height
     
     img = 'stars_clean.png'    
-    x = np.array(Image.open(img).resize((m, n)).convert("RGB"))/256.0
+    x = np.array(Image.open(img).resize((n, m)).convert("RGB"))/256.0
     
-    model = get_model()
-    model.save('saved_model/poisson_model')
+    #model = get_model()
+    #model.save('saved_model/poisson_model')
     
     model = train( )
     model.save('saved_model/poisson_model')
-    #new_model = tf.keras.models.load_model('saved_model/my_model')
-    
-    
-    
-    
-    x = x.reshape((1,m,n,3))
+    #new_model = tf.keras.models.load_model('saved_model/my_model') 
+       
+    x = np.expand_dims(x, axis=0)
     y = model.predict(x)
-    y = x.reshape((m,n,3))
+    y = np.squeeze(y, axis=0)
     my_img_show(y, "res")
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
